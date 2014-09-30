6bb7b5e23507036ebdd9fcec6845c63cccb3c6f7
Paul Arterburn <paul@brandfolder.com>
2014-09-29 18:37:27 -0600

    Fix for latest entry

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,7 @@
 class WelcomeController < ApplicationController
   def index
     if current_user
-      @last_entry = Entry.where(:user_id => current_user).sort_by(&:date).reverse.last
-      @entries = Entry.where(:user_id => current_user).sort_by(&:date).reverse
+      @last_entry = Entry.where(:user_id => current_user).sort_by(&:date).last
     end
   end
 end
