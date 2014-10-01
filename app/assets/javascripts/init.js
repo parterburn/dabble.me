2fc9ecd654591c21bc201351a074e96a9e87fee2
Paul Arterburn <paul@brandfolder.com>
2014-09-30 23:51:37 -0600

    Remove focus from user settings

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -5,10 +5,6 @@
 
   DABBLE.init = function(){
 
-    $('.j-screenshots a').click(function (e) {
-      $('#myModal img').attr('src', $(this).attr('data-img-url'));
-    });
-
     window.onscroll = function(ev) {
       if ((window.innerHeight + window.scrollY) >= 1600) {
         $(".j-back-to-top").fadeIn();
