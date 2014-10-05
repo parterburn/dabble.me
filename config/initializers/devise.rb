506d353658d6a0c21b113b175088b2fd088b1753
Paul Arterburn <paul@brandfolder.com>
2014-10-05 17:19:41 -0600

    Style

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -10,7 +10,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'Dabble Me <pleasereply@dabble.me>'
+  config.mailer_sender = 'Dabble Me <hello@dabble.me>'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
