76f39147596eff0241862788b9f0d621d2689005
Paul Arterburn <paul@brandfolder.com>
2014-10-12 15:28:58 -0600

    Better onboarding

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,4 +1,7 @@
 class UserMailer < ActionMailer::Base
+  helper.extend(ApplicationHelper)
+  include FilepickerRails::ApplicationHelper
+    
   default from: "Dabble Me <hello@#{ENV['MAIN_DOMAIN']}>"
  
   def welcome_email(user)
@@ -8,6 +11,8 @@ class UserMailer < ActionMailer::Base
 
  def second_welcome_email(user)
     @user = user
+    @first_entry = user.entries.first
+    @first_entry_filepicker_url = filepicker_image_url(@first_entry.image_url, w: 300, h: 300, fit: 'max', cache: true, rotate: :exif) if @first_entry.image_url.present?
     mail(from: "Dabble Me <#{@user.user_key}@#{ENV['SMTP_DOMAIN']}>", to: @user.email, subject: "Congrats on writing your first entry!")
   end
 end
