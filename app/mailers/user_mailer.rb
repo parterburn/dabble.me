76a85ef7bad933be2388af718a8a7c3d1795c312
Paul Arterburn <paul@brandfolder.com>
2014-10-27 23:59:40 -0600

    Better variable scopes

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,13 +6,13 @@ class UserMailer < ActionMailer::Base
  
   def welcome_email(user)
     @user = user
-    mail(from: "Dabble Me <#{@user.user_key}@#{ENV['SMTP_DOMAIN']}>", to: @user.email, subject: "Let's write your first Dabble Me entry")
+    mail(from: "Dabble Me <#{user.user_key}@#{ENV['SMTP_DOMAIN']}>", to: user.email, subject: "Let's write your first Dabble Me entry")
   end
 
  def second_welcome_email(user)
     @user = user
     @first_entry = user.entries.first
     @first_entry_filepicker_url = filepicker_image_url(@first_entry.image_url, w: 300, h: 300, fit: 'max', cache: true, rotate: :exif) if @first_entry.image_url.present?
-    mail(from: "Dabble Me <#{@user.user_key}@#{ENV['SMTP_DOMAIN']}>", to: @user.email, subject: "Congrats on writing your first entry!")
+    mail(from: "Dabble Me <#{user.user_key}@#{ENV['SMTP_DOMAIN']}>", to: user.email, subject: "Congrats on writing your first entry!")
   end
 end
