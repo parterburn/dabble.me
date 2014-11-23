343af4de65176a74c5411efeb170de41d91154e1
Paul Arterburn <paul@brandfolder.com>
2014-11-23 12:37:51 -0700

    Fix for doubles

--- lib/email_processor.rb
+++ lib/email_processor.rb
@@ -40,8 +40,8 @@ class EmailProcessor
           end
         end
 
-        @body = unfold_paragraphs(@body) unless @from.include?("yahoo.com") #fix wrapped plain text, but yahoo messes this up
         @body.gsub!(/\n\n\n/, "\n\n \n\n") #allow double line breaks
+        @body = unfold_paragraphs(@body) unless @from.include?("yahoo.com") #fix wrapped plain text, but yahoo messes this up
         @body = ActionController::Base.helpers.simple_format(@body) #format the email body coming in to basic HTML
         @body.gsub!(/\*([a-zA-Z0-9]+[a-zA-Z0-9 ]*[a-zA-Z0-9]+)\*/i, '<b>\1</b>') #bold when bold needed
         @body.gsub!(/\[image\:\ Inline\ image\ [0-9]{1,2}\]/, "") #remove "inline image" text
