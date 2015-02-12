9f404572633528a419802cb4ce863a773dc8e893
Paul Arterburn <paul@brandfolder.com>
2015-02-12 09:59:38 -0700

    Fix 500

--- config/environment.rb
+++ config/environment.rb
@@ -5,9 +5,3 @@ require File.expand_path('../application', __FILE__)
 Rails.application.initialize!
 
 Mime::Type.register "text/txt", :txt
-
-if Rails.env.development?
-  Rails.logger = Le.new("#{ENV['LOGENTRIES_KEY']}", true)
-else
-  Rails.logger = Le.new("#{ENV['LOGENTRIES_KEY']}", local: true)
-end
