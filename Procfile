85631e57bac4a4f6dfa88d96d5fc7d05a5eb96df
Paul Arterburn <paul.arterburn@brandfolder.com>
2015-05-22 01:55:37 -0600

    Fix sidekiq

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web: bundle exec puma -p $PORT -C config/puma.rb
-worker: bundle exec sidekiq
+worker: bundle exec sidekiq -e production -C config/sidekiq.yml
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,13 @@
+require 'sidekiq'
+
 Sidekiq.configure_client do |config|
-  config.redis = { namespace: 'dabbleme' }
+  config.redis = { :size => 1 }
+end
+
+Sidekiq.configure_server do |config|
+  # The config.redis is calculated by the 
+  # concurrency value so you do not need to 
+  # specify this. For this demo I do 
+  # show it to understand the numbers
+  config.redis = { :size => 9 }
 end
