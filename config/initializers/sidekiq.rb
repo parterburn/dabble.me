3a7ff51873bc210bed27fda146559d9f9f3404e7
Paul Arterburn <paul.arterburn@brandfolder.com>
2015-05-22 01:50:01 -0600

    Fix sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,3 @@
 Sidekiq.configure_client do |config|
-  if ENV["REDISCLOUD_URL"]
-    config.redis = { url: ENV["REDISCLOUD_URL"], namespace: 'dabbleme' }
-  else
-    config.redis = { namespace: 'dabbleme' }    
-  end
+  config.redis = { namespace: 'dabbleme' }
 end
