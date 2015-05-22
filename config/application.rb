83953d704063b18e0400448acba75d4e117c6df7
Paul Arterburn <paul.arterburn@brandfolder.com>
2015-05-21 23:10:27 -0600

    Fix for Heroku

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,8 @@ module Dabbleme
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = 'UTC'
 
+    config.serve_static_assets = true
+
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
