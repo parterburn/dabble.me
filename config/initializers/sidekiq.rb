ebd70673d3bdc8b97400d290943981d6ef1a1217
Paul Arterburn <paul.arterburn@brandfolder.com>
2015-05-22 01:58:35 -0600

    Fix sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,11 +3,3 @@ require 'sidekiq'
 Sidekiq.configure_client do |config|
   config.redis = { :size => 1 }
 end
-
-Sidekiq.configure_server do |config|
-  # The config.redis is calculated by the 
-  # concurrency value so you do not need to 
-  # specify this. For this demo I do 
-  # show it to understand the numbers
-  config.redis = { :size => 9 }
-end
