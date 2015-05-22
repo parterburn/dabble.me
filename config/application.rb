28c2e1c5c676821491fd17d312fc75baf88297a1
Paul Arterburn <paul.arterburn@brandfolder.com>
2015-05-21 23:13:58 -0600

    serve_static_files

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module Dabbleme
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = 'UTC'
 
-    config.serve_static_assets = true
+    config.serve_static_files = true
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
