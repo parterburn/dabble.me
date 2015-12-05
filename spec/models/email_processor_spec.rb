1a8f13772f2785d12ae994c58fd5937da5fa2cd4
Paul Arterburn <paularterburn@gmail.com>
2015-12-05 16:09:17 -0700

    Test emails

---
+++ spec/models/email_processor_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+require_relative "../../lib/email_processor"
+
+describe EmailProcessor do
+  include_context 'has all objects'
+
+  describe "#process" do
+    it "creates an entry based on the email token" do
+      email = FactoryGirl.build(
+        :email,
+        to: [{ token: user.user_key, host: ENV['SMTP_DOMAIN'], email: "#{user.user_key}@#{ENV['SMTP_DOMAIN']}"}],
+        body: "I am great"
+      )
+
+      EmailProcessor.new(email).process
+      expect(user.entries.first.body).to eq("I am great\n")
+    end
+
+    it "creates an entry from email if the token is wrong" do
+      email = FactoryGirl.build(
+        :email,
+        from: ({ email: user.email }),
+        to: [{ token: "WRONG", host: ENV['SMTP_DOMAIN'], email: "WRONG@#{ENV['SMTP_DOMAIN']}"}],
+        body: "I am great"
+      )      
+
+      EmailProcessor.new(email).process
+      expect(user.entries.first.body).to eq("I am great\n")
+    end
+  end
+end
