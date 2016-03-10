e9eb037e19ccd40e0c1a0128f207ccf26d1903d2
Paul Arterburn <paul.arterburn@brandfolder.com>
2016-03-09 19:44:54 -0700

    Add Categories

--- app/mailers/entry_mailer.rb
+++ app/mailers/entry_mailer.rb
@@ -8,6 +8,7 @@ class EntryMailer < ActionMailer::Base
     @random_entry_filepicker_url = filepicker_image_url(@random_entry.image_url, w: 300, h: 300, fit: 'max', cache: true, rotate: :exif) if @random_entry.present? && @random_entry.image_url.present?
     @random_inspiration = random_inspiration
 
+    headers['X-Mailgun-Tag'] = 'Entry'
     mail from: "Dabble Me <#{user.user_key}@#{ENV['SMTP_DOMAIN']}>",
          to: "#{user.full_name} <#{user.email}>",
          subject: "It's #{Time.now.in_time_zone(user.send_timezone).strftime('%A, %b %-d')} - How did your day go?"
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,6 +7,7 @@ class UserMailer < ActionMailer::Base
   def welcome_email(user)
     @user = user
     @user.increment!(:emails_sent)
+    headers['X-Mailgun-Tag'] = 'Welcome'
     mail(from: "Dabble Me <#{user.user_key}@#{ENV['SMTP_DOMAIN']}>", to: user.email, subject: "Let's write your first Dabble Me entry")
   end
 
@@ -15,18 +16,21 @@ class UserMailer < ActionMailer::Base
     @user.increment!(:emails_sent)
     @first_entry = user.entries.first
     @first_entry_filepicker_url = filepicker_image_url(@first_entry.image_url, w: 300, h: 300, fit: 'max', cache: true, rotate: :exif) if @first_entry.present? && @first_entry.image_url.present?
+    headers['X-Mailgun-Tag'] = 'Welcome'
     mail(from: "Dabble Me <#{user.user_key}@#{ENV['SMTP_DOMAIN']}>", to: user.email, subject: 'Congrats on writing your first entry!')
   end
 
   def thanks_for_paying(user)
     @user = user
     @user.increment!(:emails_sent)
+    headers['X-Mailgun-Tag'] = 'Thanks'
     mail(to: user.email, subject: 'Thanks for subscribing to Dabble Me PRO!')
   end
 
   def downgraded(user)
     @user = user
     @user.increment!(:emails_sent)
+    headers['X-Mailgun-Tag'] = 'Downgraded'
     mail(to: user.email, subject: '[ACTION REQUIRED] Account Downgraded')
   end
 
