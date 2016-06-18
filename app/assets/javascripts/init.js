d501875ecf898c50cf15a53c80d487c37829508e
Paul Arterburn <paularterburn@gmail.com>
2016-06-18 12:51:54 -0600

    Scroll up navbar

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -4,14 +4,47 @@
   DABBLE.pages = DABBLE.pages || {};
 
   DABBLE.init = function(){
+    var didScroll;
+    var lastScrollTop = 0;
+    var delta = 20;
+    var navbarHeight = $('.navbar-fixed-top').outerHeight();
 
-    window.onscroll = function(ev) {
-      if ((window.innerHeight + window.scrollY) >= 1600) {
-        $(".j-back-to-top").fadeIn();
-      } else {
-        $(".j-back-to-top").fadeOut();
+    $(window).scroll(function(event){
+        didScroll = true;
+    });
+
+    setInterval(function() {
+      if (didScroll) {
+        hasScrolled();
+        didScroll = false;
       }
-    };
+    }, 250);
+
+    function hasScrolled() {
+        var st = $(this).scrollTop();
+        
+        // Make sure they scroll more than delta
+        if(Math.abs(lastScrollTop - st) <= delta)
+            return;
+        
+        // If they scrolled down and are past the navbar, add class .nav-up.
+        // This is necessary so you never see what is "behind" the navbar.
+        if (st > lastScrollTop && st > navbarHeight && $(window).width() > 768){
+            // Scroll Down
+            $('.navbar-fixed-top').addClass('nav-up');
+            $(".j-back-to-top").fadeOut();
+        } else {
+            // Scroll Up
+            if(st + $(window).height() < $(document).height()) {
+                $('.navbar-fixed-top').removeClass('nav-up');
+                if ((window.innerHeight + window.scrollY) >= 1600 && $(window).width() > 768) {
+                  $(".j-back-to-top").fadeIn();
+                }
+            }
+        }
+        
+        lastScrollTop = st;
+    }
 
     $("[rel='popover']").popover({
       trigger: "hover",
