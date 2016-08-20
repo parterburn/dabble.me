d7a732e6c62c762eeb138c4093b2bbcf97504724
Paul Arterburn <paularterburn@gmail.com>
2016-08-20 12:18:54 -0600

    Update on Devise

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -31,7 +31,7 @@ class RegistrationsController < Devise::RegistrationsController
     if successfully_updated
       set_flash_message :notice, :updated
       # Sign in the user bypassing validation in case their password changed
-      sign_in @user, bypass: true
+      bypass_sign_in @user
       redirect_to edit_user_registration_path
     else
       flash[:alert] = flash[:alert].to_a.concat resource.errors.full_messages
