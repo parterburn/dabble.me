b7752ce9f50a86688742bc921519b4c9f843568f
Paul Arterburn <paularterburn@gmail.com>
2016-10-25 16:47:35 -0600

    Fix search 404

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -4,7 +4,7 @@ class SearchesController < ApplicationController
   def show
     if current_user.is_free?
       @search = Search.new(search_params)
-    elsif search_params[:term].include?(" OR ")
+    elsif search_params[:term].present? && search_params[:term].include?(" OR ")
       @search = Search.new(search_params)
       filter_names = search_params[:term].split(' OR ')
       cond_text = filter_names.map{|w| "LOWER(entries.body) like ?"}.join(" OR ")
@@ -15,15 +15,13 @@ class SearchesController < ApplicationController
       @entries = @search.entries
     end
 
-    if @entries.present?
-      hashtagged_entries = current_user.entries.where("entries.body ~ '(#[a-zA-Z0-9_]+)'")
-      @hashtags = []
-      hashtagged_entries.each do |entry|
-        entry.body.scan(/(<a[^>]*>.*?< ?\/a ?>)|(#[0-9]+\W)|(#[a-zA-Z0-9_]+)/) { |m| @hashtags << m[2] }
-      end
-      @hashtags.compact!
-      @hashtags = @hashtags.map{|i| i.downcase}.uniq.sort_by!{ |m| m.downcase }.inject({}) {|accu, uni| accu.merge({ uni => @hashtags.select{|i| i.downcase == uni.downcase } })}
+    hashtagged_entries = current_user.entries.where("entries.body ~ '(#[a-zA-Z0-9_]+)'")
+    @hashtags = []
+    hashtagged_entries.each do |entry|
+      entry.body.scan(/(<a[^>]*>.*?< ?\/a ?>)|(#[0-9]+\W)|(#[a-zA-Z0-9_]+)/) { |m| @hashtags << m[2] }
     end
+    @hashtags.compact!
+    @hashtags = @hashtags.map{|i| i.downcase}.uniq.sort_by!{ |m| m.downcase }.inject({}) {|accu, uni| accu.merge({ uni => @hashtags.select{|i| i.downcase == uni.downcase } })}
   end
 
   private
