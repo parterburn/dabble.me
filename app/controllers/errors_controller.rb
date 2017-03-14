4323d838f8512b277ffb511a490eb222cb6f489e
Paul Arterburn <paularterburn@gmail.com>
2017-03-14 13:42:39 -0600

    404, don't 500

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,9 +1,10 @@
 class ErrorsController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: :not_found
   def not_found
     respond_to do |format|
       format.html { render status: 404 }
     end
   rescue ActionController::UnknownFormat
-    render status: 404, text: "nope"
+    render status: 404, text: "This page does not exist. Looking for VidCast? It moved to https://vidcast.dabble.me/.".html_safe
   end
 end
