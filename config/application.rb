148abb4266e3adf9742338996383cc539217f0ad
Paul Arterburn <paularterburn@gmail.com>
2018-06-19 19:22:00 -0600

    Fix paths

--- config/application.rb
+++ config/application.rb
@@ -44,13 +44,11 @@ module Dabbleme
 
     config.assets.precompile << Proc.new do |path|
           if path =~ /\.(css|js)\z/
-            full_path = Rails.application.assets_manifest.find_sources.resolve(path)
+            full_path = Rails.application.assets_manifest.find_sources(path)
             app_assets_path = Rails.root.join('app', 'assets').to_path
             if full_path.starts_with? app_assets_path
-              puts "including asset: " + full_path
               true
             else
-              puts "excluding asset: " + full_path
               false
             end
           else
