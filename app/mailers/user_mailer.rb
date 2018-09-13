7e6479b3baf73ac3c2e53fce01b3ce0b42477c21
Paul Arterburn <paularterburn@gmail.com>
2018-09-12 21:59:06 -0600

    Login email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -13,7 +13,7 @@ class UserMailer < ActionMailer::Base
   def confirm_user(user)
     @user = user
     return nil unless @user.paranoid_verification_code.present?
-    email = mail(to: user.cleaned_to_address, subject: "Login confirmation code")
+    email = mail(to: user.cleaned_to_address, subject: "Login code for Dabble Me")
     email.mailgun_options = {tag: 'Login Code'}
   end
 
