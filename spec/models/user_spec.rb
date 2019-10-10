5975e4797a8380904f0ca9dbd3c071830e499f55
Paul Arterburn <paularterburn@gmail.com>
2019-10-10 16:48:02 -0600

    Update tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,14 +19,14 @@ describe User do
     end
 
     it "returns same day, way back" do
-      entry_date = Date.parse("2018-10-5")
+      entry_date = Date.parse("2019-10-5")
       past_entry = user.entries.create(date: Date.parse("2002-10-5"), body: "Hi from way back.")
       user.way_back_past_entries = true
       expect(user.random_entry(entry_date)).to eq(past_entry)
     end
 
     it "returns 1 year ago" do
-      entry_date = Date.parse("2018-10-5")
+      entry_date = Date.parse("2019-10-5")
       past_entry = user.entries.create(date: Date.parse("2017-10-5"), body: "Hi from 1 year back.")
       user.way_back_past_entries = false
       expect(user.random_entry(entry_date)).to eq(past_entry)
@@ -34,7 +34,7 @@ describe User do
 
     it "returns 1 month ago" do
       user.emails_sent = 3
-      entry_date = Date.parse("2018-10-5")
+      entry_date = Date.parse("2019-10-5")
       past_entry = user.entries.create(date: Date.parse("2018-9-5"), body: "Hi from 1 month back.")
       expect(user.random_entry(entry_date)).to eq(past_entry)
     end
@@ -42,7 +42,7 @@ describe User do
     it "returns 1 week ago" do
       user.way_back_past_entries = false
       user.emails_sent = 5
-      entry_date = Date.parse("2018-10-5")
+      entry_date = Date.parse("2019-10-5")
       past_entry = user.entries.create(date: Date.parse("2018-9-28"), body: "Hi from 1 week back.")
       expect(user.random_entry(entry_date)).to eq(past_entry)
     end    
@@ -50,7 +50,7 @@ describe User do
     it "returns random from way back" do
       user.emails_sent = 2
       user.way_back_past_entries = true
-      entry_date = Date.parse("2018-10-5")
+      entry_date = Date.parse("2019-10-5")
       (1..31).each do |i|
         user.entries.create(date: Date.parse("2015-1-#{i}"), body: "Hi from way back.")
       end
@@ -59,15 +59,15 @@ describe User do
 
     it "returns pure random, including way back" do
       user.way_back_past_entries = true
-      entry_date = Date.parse("2018-10-5")
-      user.entries.create(date: Date.parse("2015-1-15"), body: "hi.")
+      entry_date = Date.parse("2019-10-5")
+      user.entries.create(date: Date.parse("2016-1-15"), body: "hi.")
       expect(user.random_entry(entry_date).body).to eq("hi.")
     end
 
     it "returns pure random, not way back" do
       user.way_back_past_entries = false
-      entry_date = Date.parse("2018-10-5")
-      user.entries.create(date: Date.parse("2015-1-15"), body: "Hi from way back.")
+      entry_date = Date.parse("2019-10-5")
+      user.entries.create(date: Date.parse("2016-1-15"), body: "Hi from way back.")
       user.entries.create(date: Date.parse("2018-10-3"), body: "Hi from a few days ago.")
       expect(user.random_entry(entry_date).date.year).to_not eq(2015)
     end           
