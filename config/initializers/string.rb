8175b85c14f465ea7398e7abf52dc4c3f723f4ae
Paul Arterburn <paularterburn@gmail.com>
2020-03-25 23:31:40 -0600

    Better encoding

--- config/initializers/string.rb
+++ config/initializers/string.rb
@@ -1,7 +1,22 @@
 class String
-  JS_ESCAPE_MAP   =   { '\\' => '\\\\', '</' => '<\/', "\r\n" => '\n', "\n" => '\n', "\r" => '\n', '"' => '\\"', "'" => "\\'" }
+  JS_ESCAPE_MAP = {
+    '\\'    => '\\\\',
+    "</"    => '<\/',
+    "\r\n"  => '\n',
+    "\n"    => '\n',
+    "\r"    => '\n',
+    '"'     => '\\"',
+    "'"     => "\\'",
+    ";"     => "%3B",
+    "`"     => "\\`",
+    "$"     => "\\$"
+  }
+
+  JS_ESCAPE_MAP[(+"\342\200\250").force_encoding(Encoding::UTF_8).encode!] = "&#x2028;"
+  JS_ESCAPE_MAP[(+"\342\200\251").force_encoding(Encoding::UTF_8).encode!] = "&#x2029;"
 
   def escape_javascript
-    self&.gsub(/(\|<\/|\r\n|\342\200\250|\342\200\251|[\n\r"'])/u) {|match| JS_ESCAPE_MAP[match] }
+    result = self.gsub(/(\\|<\/|\r\n|\342\200\250|\342\200\251|[\n\r"';]|[`]|[$])/u) { |match| JS_ESCAPE_MAP[match] }
+    self.html_safe? ? result.html_safe : result
   end
 end
