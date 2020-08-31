efdda9da699450cbec258f26a58bb0ac69db63d5
Paul Arterburn <paularterburn@gmail.com>
2020-08-30 21:33:57 -0600

    Fix for undefined method

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,7 @@ class UserMailer < ActionMailer::Base
   helper.extend(ApplicationHelper)
   add_template_helper(ApplicationHelper)
   add_template_helper(EntriesHelper)
+  helper :application
 
   default from: "Dabble Me Support <hello@#{ENV['MAIN_DOMAIN']}>"
 
