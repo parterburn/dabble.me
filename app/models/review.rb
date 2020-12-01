ee30f5c14d9eb148c6abd4e319ab1702d5044fe9
Amiran <amiranlevanovich@gmail.com>
2020-12-01 23:12:11 +0300

    update  review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,17 @@
-class Review < ApplicationRecord
+class Review < ActiveRecord::Base
+  STATUSES = %w[new approved changes_requested].freeze
+
   belongs_to :entry
   belongs_to :user
 
-  validates :review_body, length: { maximum: 140 }
+  validates :review_body, length: { maximum: 140 },
+                          presence: true, unless: -> { new? }
+
+  def approved?
+    status == 'approved'
+  end
+
+  def new?
+    status == 'new'
+  end
 end
