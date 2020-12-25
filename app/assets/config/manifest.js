3309aae5b48025c4b8b04701bb3f5a9f1c435ac1
Paul Arterburn <paularterburn@gmail.com>
2020-12-24 22:19:42 -0700

    Add new book cover to manifest

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,7 +1,7 @@
 //= link dabble_me_pencil_400.png
 //= link dabble_me_pencil_800.png
 //= link dabble_me_pencil_logo.png
-//= link moment-of-lift.jpg
+//= link caste.jpg
 //= link ohlifelogo.gif
 //= link born-a-crime.jpg
 //= link bg_pattern.gif
