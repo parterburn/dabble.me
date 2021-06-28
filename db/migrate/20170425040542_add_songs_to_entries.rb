3f9e952d94eeb42efb0ce6caa9289f9a3325c797
Paul Arterburn <paularterburn@gmail.com>
2021-06-28 12:17:36 -0600

    Fix migration

--- db/migrate/20170425040542_add_songs_to_entries.rb
+++ db/migrate/20170425040542_add_songs_to_entries.rb
@@ -1,5 +1,5 @@
-class AddSongsToEntries < ActiveRecord::Migration[4.2]
+class AddSongsToEntries < ActiveRecord::Migration[5.0]
   def change
-    add_column :entries, :songs, :jsonb, default: []
+    add_column :entries, :songs, :jsonb, default: [], array:true
   end
 end
