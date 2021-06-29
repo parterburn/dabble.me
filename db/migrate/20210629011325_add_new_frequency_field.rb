2d021e53bdb3f03abd5bff6ee8198261890870a4
Paul Arterburn <paularterburn@gmail.com>
2021-06-28 20:25:58 -0600

    Don't change column on migration

--- db/migrate/20210629011325_add_new_frequency_field.rb
+++ db/migrate/20210629011325_add_new_frequency_field.rb
@@ -1,5 +1,5 @@
 class AddNewFrequencyField < ActiveRecord::Migration[6.0]
   def change
-    change_column :users, :frequency, :text, array: true, default: ["Sun"], null: true
+    # change_column :users, :frequency, :text, array: true, default: ["Sun"], null: true
   end
 end
