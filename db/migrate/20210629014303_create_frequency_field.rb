1ec2342d7ca5128144d01109ac50a05879997805
Paul Arterburn <paularterburn@gmail.com>
2021-06-28 20:50:07 -0600

    Create column

--- db/migrate/20210629014303_create_frequency_field.rb
+++ db/migrate/20210629014303_create_frequency_field.rb
@@ -1,21 +1,5 @@
 class CreateFrequencyField < ActiveRecord::Migration[6.0]  
   def up
     add_column :users, :frequency_upd, :text, array: true, default: ["Sun"]
-
-    User.find_each do |user|
-      freqs = []
-      f = user.read_attribute(:frequency).to_s
-
-      freqs << "Sun" if "Sun".in?(f)
-      freqs << "Mon" if "Mon".in?(f)
-      freqs << "Tue" if "Tue".in?(f)
-      freqs << "Wed" if "Wed".in?(f)
-      freqs << "Thu" if "Thu".in?(f)
-      freqs << "Fri" if "Fri".in?(f)
-      freqs << "Sat" if "Sat".in?(f)
-
-      user.frequency_upd = freqs
-      user.save(validate: false)
-    end
   end
 end
