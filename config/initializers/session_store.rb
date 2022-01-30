342a216dec252c53ea3845d0009d4552797ea322
Paul Arterburn <paularterburn@gmail.com>
2022-01-30 14:19:30 -0700

    Set devise rememberable to match session cookie

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_dabbleme_session'
+Rails.application.config.session_store :cookie_store, key: "_dabbleme_session", expire_after: 2.weeks, same_site: :lax
