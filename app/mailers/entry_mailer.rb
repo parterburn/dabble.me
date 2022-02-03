49ac15156b13cfb3a48d7247dd8d9a2a5cc11e05
Paul Arterburn <paularterburn@gmail.com>
2022-02-02 17:43:41 -0700

    Allow date overrides for catchup

--- app/mailers/entry_mailer.rb
+++ app/mailers/entry_mailer.rb
@@ -2,18 +2,19 @@ class EntryMailer < ActionMailer::Base
   helper.extend(ApplicationHelper)
   add_template_helper(EntriesHelper)
 
-  def send_entry(user, random_inspiration)
+  def send_entry(user, random_inspiration, send_day: nil)
+    @send_day = send_day.presence || Time.now.in_time_zone(user.send_timezone)
     @random_inspiration = random_inspiration
     @user = user
     @user.increment!(:emails_sent)
-    @random_entry = user.random_entry(Time.now.in_time_zone(user.send_timezone).strftime('%Y-%m-%d'))
+    @random_entry = user.random_entry(@send_day.strftime('%Y-%m-%d'))
     if @random_entry.present?
       @random_entry_image_url = @random_entry.image_url_cdn
     end
 
     email = mail  from: "Dabble Me ✏ <#{user.user_key}@#{ENV['SMTP_DOMAIN']}>",
                   to: "#{user.cleaned_to_address}",
-                  subject: "It's #{Time.now.in_time_zone(user.send_timezone).strftime('%A, %b %-d')}. How was your day?",
+                  subject: "It's #{@send_day.strftime('%A, %b %-d')}. How was your day?",
                   html: (render_to_string(template: '../views/entry_mailer/send_entry.html')).to_str,
                   text: (render_to_string(template: '../views/entry_mailer/send_entry.text')).to_str
 
