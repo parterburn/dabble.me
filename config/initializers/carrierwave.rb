7ece271be9588cca435c80986225461da49399be
Paul Arterburn <paularterburn@gmail.com>
2022-03-07 23:12:35 -0700

    Force inline on all files uploaded

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,7 +7,7 @@ CarrierWave.configure do |config|
 
   config.fog_directory = ENV["AWS_BUCKET"]
   config.fog_public = true
-  config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
+  config.fog_attributes = { content_disposition: "inline", cache_control: "public, max-age=#{365.days.to_i}" }
 end
 
 module CarrierWave
