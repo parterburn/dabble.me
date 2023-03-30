bb18dbda07d78e13c897f65b0a4468c2ee58b2b6
Paul Arterburn <paularterburn@gmail.com>
2023-03-30 16:32:41 -0600

    Only run initializer in test

--- config/initializers/email_spec.rb
+++ config/initializers/email_spec.rb
@@ -1,9 +1,11 @@
-ActiveSupport.on_load(:action_mailer) do
-  # fix https://github.com/email-spec/email-spec/issues/220
-  module EmailSpec
-    module Helpers; end
-  end
+if Rails.env.test?
+  ActiveSupport.on_load(:action_mailer) do
+    # fix https://github.com/email-spec/email-spec/issues/220
+    module EmailSpec
+      module Helpers; end
+    end
 
-  # fix warning "Neither Pony nor ActionMailer appear to be loaded so email-spec is requiring ActionMailer."
-  require 'email_spec'
+    # fix warning "Neither Pony nor ActionMailer appear to be loaded so email-spec is requiring ActionMailer."
+    require 'email_spec'
+  end
 end
