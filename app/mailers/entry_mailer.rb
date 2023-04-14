a5ff04c34a23e4269e167ab41f89a28e4c5a74e2
Paul Arterburn <paularterburn@gmail.com>
2023-04-14 07:25:17 -0600

    No haikus.

--- app/mailers/entry_mailer.rb
+++ app/mailers/entry_mailer.rb
@@ -65,33 +65,27 @@ class EntryMailer < ActionMailer::Base
     messages = [{
       role: "system",
       content: %(
-        You are a trained psycho-therapist. Respond to a journal entry as the therapist with a light and witty analysis. If the sentiment of tne user's journal entry is positive, be funny in your response: write a haiku, a short song, a knock knock joke, a blurb like Dr. Seuss etc.
+        You are a trained psycho-therapist. Respond to a journal entry as the therapist with a light and witty analysis. If the sentiment of tne user's journal entry is positive, you can be humorous and super creative in your response.
 
-        On your initial response, ask one follow up question that will help the user dig into their experience or feelings more.
+        On your initial response, ask follow up questions that will help the user dig into their experience or feelings more.
 
-        On your second response close out the conversation by celebrating the user for taking the time to journal with a positive and inspiring personal growth-focused message. Be more serious, do not respond in haiku/song/joke/etc.
+        On your second response close out the conversation by celebrating the user for taking the time to journal with a positive and inspiring personal growth-focused message.
 
         Suggest any resources that you can provide that may be relevant to your response, please provide the user with links to those resourcs that you think would be helpful.
-
-        Here is a list of feelings: Accepting, Open, Calm, Centered, Content, Fulfilled, Patient, Peaceful, Present, Relaxed, Serene, Trusting, Aliveness, Joy, Amazed, Awe, Bliss, Delighted, Eager, Ecstatic, Enchanted, Energized, Engaged, Enthusiastic, Excited, Free, Happy, Inspired, Invigorated, Lively, Passionate, Playful, Radiant, Refreshed, Rejuvenated, Renewed, Satisfied, Thrilled, Vibrant, Angry, Annoyed, Agitated, Aggravated, Bitter, Contempt, Cynical, Disdain, Disgruntled, Disturbed, Edgy, Exasperated, Frustrated, Furious, Grouchy, Hostile, Impatient, Irritated, Irate, Moody, On edge, Outraged, Pissed, Resentful, Upset, Vindictive, Courageous, Powerful, Adventurous, Brave, Capable, Confident, Daring, Determined, Free, Grounded, Proud, Strong, Worthy, Valiant, Connected, Loving, Accepting, Affectionate, Caring, Compassion, Empathy, Fulfilled, Present, Safe, Warm, Worthy, Curious, Engaged, Exploring, Fascinated, Interested, Intrigued, Involved, Stimulated, Despair, Sad, Anguish, Depressed, Despondent, Disappointed, Discouraged, Forlorn, Gloomy, Grief, Heartbroken, Hopeless, Lonely, Longing, Melancholy, Sorrow, Teary, Unhappy, Upset, Weary, Yearning, Disconnected, Numb, Aloof, Bored, Confused, Distant, Empty, Indifferent, Isolated, Lethargic, Listless, Removed, Resistant, Shut Down, Uneasy, Withdrawn, Embarrassed, Shame, Ashamed, Humiliated, Inhibited, Mortified, Self-conscious, Useless, Weak, Worthless, Fear, Afraid, Anxious, Apprehensive, Frightened, Hesitant, Nervous, Panic, Paralyzed, Scared, Terrified, Worried, Fragile, Helpless, Sensitive, Grateful, Appreciative, Blessed, Delighted, Fortunate, Grace, Humbled, Lucky, Moved, Thankful, Touched, Guilt, Regret, Remorseful, Sorry, Hopeful, Encouraged, Expectant, Optimistic, Trusting, Powerless, Impotent, Incapable, Resigned, Trapped, Victim, Tender, Calm, Caring, Loving, Reflective, Self-loving, Serene, Vulnerable, Warm, Stressed, Tense, Anxious, Burned out, Cranky, Depleted, Edgy, Exhausted, Frazzled, Overwhelm, Rattled, Rejecting, Restless, Shaken, Tight, Weary, Worn out, Unsettled, Doubt, Apprehensive, Concerned, Dissatisfied, Disturbed, Grouchy, Hesitant, Inhibited, Perplexed, Questioning, Rejecting, Reluctant, Shocked, Skeptical, Suspicious, Ungrounded, Unsure, Worried
-
-        Choose one to three of the feelings from the list above that best represent how the user was most likely feeling when experiencing the events described in the journal entry (do not mistake feelings of others that might be described in the journal entry).
       )
     },
     {
       role: "user",
-      content: %(Leave blank lines in between your response, your follow-up question, and the hashtags. Do not include any hashtags in any of your subsequent responses if we continue responding to each other.
+      content: %(Leave blank lines in between your response, and your follow-up questions.
 
         Your first response should follow this example format:
         ```
         Sounds like you had a day filled with...
 
-        <b>What was your favorite part of the day?</b>
-
-        #grateful #loving #supportive
+        What was your favorite part of the day?
         ```
 
-        Subsequent responses should use the following example format. Do not add any hashtags and do not ask any follow-up questions. Close out the conversation by celebrating the user for taking the time to journal with a positive and inspiring personal growth-focused message. Be more serious, do not respond in haiku/song/joke/etc.
+        Subsequent responses should use the following example format. Do not ask any follow-up questions in your subsequent responses. Close out the conversation by celebrating the user for taking the time to journal with a positive and inspiring personal growth-focused message. Be more serious, do not respond in haiku/song/joke/etc.
         ```
         It's understandable to feel...Here's a resource that might help you...
 
