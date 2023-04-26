391f50ee7421d0bac130dd9ead62f41bcd63f399
Paul Arterburn <paularterburn@gmail.com>
2023-04-25 21:23:42 -0600

    Test out concept of collages

--- app/models/user.rb
+++ app/models/user.rb
@@ -249,6 +249,13 @@ class User < ActiveRecord::Base
     streak
   end
 
+  def image_collage
+    last_5_images = entries.only_images.last(5).map(&:image_url_cdn)
+    return nil unless last_5_images.size > 1
+
+    "https://process.filestackapi.com/#{ENV['FILESTACK_API_KEY']}/collage=a:true,i:auto,f:[#{last_5_images[1..-1].map(&:inspect).join(',')}],w:1200,h:1200,m:10/#{last_5_images.first}"
+  end
+
   private
 
   def restrict_free_frequency
