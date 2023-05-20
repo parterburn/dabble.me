7c55f8baf0213bce89a173cedf124c39ecdb31e9
Paul Arterburn <paularterburn@gmail.com>
2023-05-20 08:58:00 -0600

    Throw more sentry errors

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,6 +1,7 @@
 if ENV["SENTRY_DSN"].present?
   Sentry.init do |config|
     config.dsn = ENV["SENTRY_DSN"]
+    config.excluded_exceptions = Sentry::Rails::IGNORE_DEFAULT - ["ActionController::BadRequest"]
     config.breadcrumbs_logger = [:active_support_logger, :http_logger]
 
     config.traces_sample_rate = 1.0
