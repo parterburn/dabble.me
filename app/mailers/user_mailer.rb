564887a6796b5a889cd274be70bfc05a9a5a3f94
Paul Arterburn <paularterburn@gmail.com>
2023-11-27 08:47:06 -0700

    Line break

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -63,7 +63,7 @@ class UserMailer < ActionMailer::Base
       end.to_sentence
     end
 
-    mail(to: "hello@#{ENV['MAIN_DOMAIN']}", subject: '[ACTION REQUIRED] Pro User Deleted', body: "Name: #{name}\nEmail: #{email}\nUser ID: #{user_id}\nPlan: #{plan}\nEntries: #{entries}\nPayhere ID: #{payhere_id}\nStripe Subscription Status: #{subscription_status}\nStripe: https://dashboard.stripe.com/customers/#{stripe_id}")
+    mail(to: "hello@#{ENV['MAIN_DOMAIN']}", subject: '[ACTION REQUIRED] Pro User Deleted', body: "Name: #{name}\nEmail: #{email}\nUser ID: #{user_id}\nPlan: #{plan}\nEntries: #{entries}\nPayhere ID: #{payhere_id}\nStripe Subscription Status: #{subscription_status}\n\nStripe: https://dashboard.stripe.com/customers/#{stripe_id}")
   end
 
   def failed_entry(user, errors, date, body)
