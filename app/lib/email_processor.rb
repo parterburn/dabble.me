8990b1555b9e501e22dff03464d4a56f365585c7
Paul Arterburn <paularterburn@gmail.com>
2023-12-19 12:01:20 -0700

    Don't store sig images

--- app/lib/email_processor.rb
+++ app/lib/email_processor.rb
@@ -51,6 +51,8 @@ class EmailProcessor
 
           # skip signature images
           next if @user.id == 293 && attachment&.original_filename == "cropped-IMG-0719-300x86.jpeg"
+          next if @user.id == 10836 && attachment&.original_filename == "B_Logo.png"
+          next if @user.id == 2541 && attachment&.original_filename == "image001.jpg"
 
           if (attachment.content_type == "application/octet-stream" || attachment.content_type =~ /^image\/(png|jpe?g|webp|gif|heic|heif)$/i || attachment.original_filename =~ /^(.+\.(heic|heif))$/i) && file_size > 20000
             valid_attachments << attachment
