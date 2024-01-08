e7a9fc76eb5ed8f1d5a474fb325b2d2a58eec113
Paul Arterburn <paularterburn@gmail.com>
2024-01-07 21:45:38 -0700

    Prevent saving signature images

--- app/lib/email_processor.rb
+++ app/lib/email_processor.rb
@@ -50,11 +50,11 @@ class EmailProcessor
           file_size = File.size?(attachment.tempfile).to_i
 
           # skip signature images
-          next if @user.id == 293 && attachment&.original_filename == "cropped-IMG-0719-300x86.jpeg"
-          next if @user.id == 10836 && attachment&.original_filename == "B_Logo.png"
-          next if @user.id == 2541 && attachment&.original_filename == "image001.jpg"
+          next if @user.id == 293 && (attachment&.original_filename.to_s == "cropped-IMG-0719-300x86.jpeg" || attachment&.filename.to_s == "cropped-IMG-0719-300x86.jpeg")
+          next if @user.id == 10836 && attachment&.original_filename.to_s == "B_Logo.png" || attachment&.filename.to_s == "B_Logo.png")
+          next if @user.id == 2541 && attachment&.original_filename.to_s == "image001.jpg" || attachment&.filename.to_s == "image001.jpg")
 
-          if (attachment.content_type == "application/octet-stream" || attachment.content_type =~ /^image\/(png|jpe?g|webp|gif|heic|heif)$/i || attachment.original_filename =~ /^(.+\.(heic|heif))$/i) && file_size > 20000
+          if (attachment.content_type == "application/octet-stream" || attachment.content_type =~ /^image\/(png|jpe?g|webp|gif|heic|heif)$/i || attachment&.original_filename.to_s =~ /^(.+\.(heic|heif))$/i || attachment&.filename.to_s =~ /^(.+\.(heic|heif))$/i) && file_size > 20000
             valid_attachments << attachment
           end
         end
