b65f1f6739bb792ea5bf25677fdac0d9404584f1
Paul Arterburn <paularterburn@gmail.com>
2024-05-15 09:31:50 -0600

    Don't crash site

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -155,7 +155,7 @@ class Entry < ActiveRecord::Base
         payload = { inputs: [ { data: { image: { url: image_url_cdn } } } ] }.to_json
         res = JSON.parse(RestClient.post(url, payload, headers))
         nsfw_percent = res.try(:[], 'outputs')&.first.try(:[], 'data').try(:[], 'concepts')&.second.try(:[], 'value')
-        if nsfw_percent >= ENV['CLARIFAI_THRESHOLD'].to_f
+        if nsfw_percent.present? && nsfw_percent >= ENV['CLARIFAI_THRESHOLD'].to_f
           Sentry.set_user(id: user.id, email: user.email)
           Sentry.set_tags(plan: user.plan)
           Sentry.capture_message("Clarifai Flagged", level: :warning, extra: { entry_id: id, nsfw_pct: "#{(nsfw_percent*100).round(1)}%", image: image_url_cdn, clarifai: res })
