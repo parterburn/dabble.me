454dcee6cc2a10364f225d95b51e3b09b8e7a0bf
Paul Arterburn <paularterburn@gmail.com>
2025-01-06 12:48:07 -0700

    Github Contribution Graph in Review

--- app/helpers/entries_helper.rb
+++ app/helpers/entries_helper.rb
@@ -10,4 +10,27 @@ module EntriesHelper
     body = Rinku.auto_link(body, :all, 'target="_blank"')
     sanitize body, tags: %w(strong em a div span ul ol li b i img br p hr u em blockquote), attributes: %w(href style src target data-content)
   end
+
+  def contribution_calendar_data(entries, year)
+    start_date = Date.new(year.to_i, 1, 1)
+    end_date = Date.new(year.to_i, 12, 31)
+
+    # Create a set of dates that have entries
+    entry_dates = entries.pluck(:date).map(&:to_date)
+
+    # Generate calendar data
+    dates = []
+    date = start_date
+
+    while date <= end_date
+      dates << {
+        date: date,
+        has_entry: entry_dates.include?(date),
+        day: date.yday
+      }
+      date += 1.day
+    end
+
+    dates
+  end
 end
