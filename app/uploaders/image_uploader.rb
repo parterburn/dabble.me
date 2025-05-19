6c7a3cc1f3eeddd362ba082636f7e77a4e5c0d69
Paul Arterburn <paularterburn@gmail.com>
2025-05-19 15:28:50 -0600

    don't override method

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -75,11 +75,10 @@ class ImageUploader < CarrierWave::Uploader::Base
     filename.gsub(/\.heic/i, ".jpg").gsub(/\.heif/i, ".jpg")
   end
 
-  def filename(original: false)
-    filename = super
-    filename = filename&.gsub(/\.heic/i, ".jpg")&.gsub(/\.heif/i, ".jpg") unless original
-    filename
-  end
+  # def filename
+  #   filename = super
+  #   filename = filename&.gsub(/\.heic/i, ".jpg")&.gsub(/\.heif/i, ".jpg")
+  # end
 
   def original_dimensions(file)
     # Add timeout protection for MiniMagick
