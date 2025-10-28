c2f4825391e2f1f652bd9952d36e293a2217899c
Paul Arterburn <paularterburn@gmail.com>
2025-10-28 09:15:11 -0600

    Update api call

--- app/models/concerns/entry/ai_tagger.rb
+++ app/models/concerns/entry/ai_tagger.rb
@@ -1,6 +1,6 @@
 class Entry::AiTagger
   SCORE_THRESHOLD = 0.35 # higher = higher confidence in match
-  BASE_URL = "https://api-inference.huggingface.co".freeze
+  BASE_URL = "https://router.huggingface.co/hf-inference/".freeze
   AI_MODEL = "/models/j-hartmann/emotion-english-distilroberta-base".freeze
   MAX_ENTRY_SIZE = 512
 
