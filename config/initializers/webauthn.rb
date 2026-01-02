10e6e9dd4451ca7a0f6e2bf58daafb2ee9021065
Paul Arterburn <paularterburn@gmail.com>
2026-01-02 01:22:08 -0700

    Update stats

--- config/initializers/webauthn.rb
+++ config/initializers/webauthn.rb
@@ -2,13 +2,13 @@ WebAuthn.configure do |config|
   # This RP ID should match the domain of your application
   if Rails.env.production?
     config.rp_id = "dabble.me"
-    config.origin = "https://dabble.me"
+    config.allowed_origins = ["https://dabble.me"]
   elsif Rails.env.development?
     config.rp_id = "localhost"
-    config.origin = "http://localhost:3000"
+    config.allowed_origins = ["http://localhost:3000"]
   else
     config.rp_id = "test.host"
-    config.origin = "http://test.host"
+    config.allowed_origins = ["http://test.host"]
   end
   config.rp_name = "Dabble Me"
 end
