c2c128f8cf8c49488d4f2947515b04d1aff0e0ca
Paul Arterburn <paularterburn@gmail.com>
2026-01-07 17:32:01 -0700

    WIP

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,34 @@
 class WelcomeController < ApplicationController
+  layout :choose_layout
+
   def index
     redirect_to latest_entry_path if user_signed_in?
     @total_entries = Entry.count
+  end
+
+  def support
+    # Support/FAQ page
+  end
+
+  def privacy
+    # Privacy policy page
+  end
+
+  def terms
+    # Terms of service page
+  end
+
+  def ohlife_alternative
+    # SEO landing page for OhLife users
+  end
+
+  private
+
+  def choose_layout
+    if user_signed_in?
+      'application'
+    else
+      'marketing'
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -51,11 +51,14 @@ Rails.application.routes.draw do
   get 'write',                          to: redirect('/entries/new')
   get 'privacy',                        to: 'welcome#privacy'
   get 'terms',                          to: 'welcome#terms'
-  get 'features',                       to: 'welcome#features'
-  get 'faqs',                           to: 'welcome#faqs'
-  get 'subscribe',                      to: 'welcome#subscribe'
-  get 'donate',                         to: redirect('/subscribe')
-  get 'pro',                            to: redirect('/subscribe')
+  get 'support',                        to: 'welcome#support'
+
+  # Redirects for old routes
+  get 'features',                       to: redirect('/#features')
+  get 'faqs',                           to: redirect('/support')
+  get 'subscribe',                      to: redirect('/#pricing')
+  get 'donate',                         to: redirect('/#pricing')
+  get 'pro',                            to: redirect('/#pricing')
   match 'payment_notify',               to: 'payments#payment_notify', via: [:post]
   get 'ohlife-alternative',             to: 'welcome#ohlife_alternative'
   post 'email_processor',               to: 'griddler/emails#create'
