6d08740b1fd0724a758a980b9bbbd316a227a72d
Paul Arterburn <paularterburn@gmail.com>
2026-04-17 21:05:48 -0600

    Refactor Sidekiq configuration to streamline cron job loading
    - Adjust `config/initializers/sidekiq.rb` to simplify the loading of cron jobs from `config/schedule.yml` by removing the conditional check for the `ENABLE_CRON_JOBS` environment variable. This change ensures that the schedule is always loaded if the file exists, improving the clarity and maintainability of the configuration.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,12 +3,10 @@ Rails.application.config.to_prepare do
 end
 
 Sidekiq.configure_server do |config|
-  if ENV["ENABLE_CRON_JOBS"] == "true"
-    schedule_file = Rails.root.join("config/schedule.yml")
+  schedule_file = Rails.root.join("config/schedule.yml")
 
-    if File.exist?(schedule_file)
-      schedule = YAML.load_file(schedule_file)
-      Sidekiq::Cron::Job.load_from_hash(schedule)
-    end
+  if File.exist?(schedule_file)
+    schedule = YAML.load_file(schedule_file)
+    Sidekiq::Cron::Job.load_from_hash(schedule)
   end
 end
