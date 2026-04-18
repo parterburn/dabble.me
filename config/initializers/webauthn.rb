446012fbf46fa3803d067b7ff21ddf0d70197f76
Paul Arterburn <paularterburn@gmail.com>
2026-04-17 20:10:20 -0600

    Update WebAuthn configuration to use environment variables for production settings
    - Modify `config/initializers/webauthn.rb` to set the RP ID and allowed origins using environment variables in the production environment. This change enhances security and flexibility by allowing the application to adapt to different production hosts without code changes.

--- config/initializers/webauthn.rb
+++ config/initializers/webauthn.rb
@@ -1,8 +1,8 @@
 WebAuthn.configure do |config|
   # This RP ID should match the domain of your application
   if Rails.env.production?
-    config.rp_id = "dabble.me"
-    config.allowed_origins = ["https://dabble.me"]
+    config.rp_id = ENV["PROD_HOST"]
+    config.allowed_origins = ["https://#{ENV["PROD_HOST"]}"]
   elsif Rails.env.development?
     config.rp_id = "localhost"
     config.allowed_origins = ["http://localhost:3000"]
